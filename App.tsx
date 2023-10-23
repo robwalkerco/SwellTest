6f9c3f95314ee3a6bd5be5622f13bb33f91d89c1
Rob Walker <hi@robwalker.co>
2023-10-23 15:57:59 +0100

    Reproduce Swell cart issue

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import swellJS from "swell-js";
+import buffer from "buffer";
+import React from "react";
+
+global.Buffer = buffer.Buffer;
+
+const STORE_ID = "";
+const PUBLIC_ID = "";
+swellJS.init(STORE_ID, PUBLIC_ID);
 
 export default function App() {
+  React.useEffect(() => {
+    const fn = async () => {
+      console.log(await swellJS.cart.get());
+    };
+    fn();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
